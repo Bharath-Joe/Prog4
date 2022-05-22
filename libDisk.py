244769c34b197b50ff44a66f9788f66d2ff8936e
Bharath1121 <bsenthil@calpoly.edu>
2022-05-22 15:54:13 -0700

    Started Phase 1

---
+++ libDisk.py
@@ -0,0 +1,21 @@
+BLOCKSIZE = 256
+
+def openDisk(filename, nBytes):
+    return 0
+
+def readBlock(disk, bNum, block):
+    return 0
+
+def writeBlock(disk, bNum, block):
+    return 0
+
+def closeDisk(disk):
+    return 0
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == '__main__':
+    main()
+    

9526def1a52ec5649d4bad48fdd2090a552b111c
Bharath1121 <bsenthil@calpoly.edu>
2022-05-25 15:17:59 -0700

    Working on Phase 1

--- libDisk.py
+++ libDisk.py
@@ -1,21 +1,62 @@
+from os.path import exists
+
+from numpy import nbytes
+
 BLOCKSIZE = 256
+diskToFile = {} # disk number: [filename, status]
+initialDiskNum = 0
+nBytesArray = {}
 
 def openDisk(filename, nBytes):
-    return 0
+    if BLOCKSIZE % nBytes != 0 or nBytes < 0:
+        print("Error")
+        return -1
+    if nBytes > 0:
+        open(filename, "w")
+        initialDiskNum += 1
+        diskToFile[initialDiskNum] = [filename, "opened"]
+        nBytesArray[initialDiskNum] = nBytes
+        return 0
+    elif nBytes == 0 and exists(filename) == False:
+        print("Error")
+        return -1
+    elif nBytes == 0:
+        open(filename, "w")
+        return 0
 
 def readBlock(disk, bNum, block):
-    return 0
+    filename = diskToFile[disk][0]
+    status = diskToFile[disk][0]
+    nybtes = nBytesArray[disk]
+    if disk in diskToFile.keys():
+        if nybtes <= bNum * BLOCKSIZE:
+            print("Error")
+            return -1
+        elif status == "opened":
+            f = open(filename, "rb")
+            f.seek(bNum * BLOCKSIZE)
+            block = f.read(BLOCKSIZE) # when would the size of block be anything less than 256
+            return 0, block
+    print("Error")
+    return -1
 
 def writeBlock(disk, bNum, block):
-    return 0
+    filename = diskToFile[disk][0]
+    status = diskToFile[disk][0]
+    nybtes = nBytesArray[disk]
+    if disk in diskToFile.keys():
+        if nybtes <= bNum * BLOCKSIZE:
+            print("Error")
+            return -1
+        if status == "opened":
+            f = open(filename, "wb")
+            f.seek(bNum * BLOCKSIZE)
+            f.write(block) # must write 256 bytes no matter the size of block ???
+            return 0
+    print("Error")
+    return -1
+
 
 def closeDisk(disk):
+    diskToFile[disk][1] = "closed"
     return 0
-
-
-def main():
-    print("Hello World!")
-
-if __name__ == '__main__':
-    main()
-    
